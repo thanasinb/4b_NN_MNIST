bcc190c3a4281e4b5e10d5430efe5ea39c1b7b69
Thanasin Bunnam <thanasin.b@gmail.com>
2022-01-21 15:30:48 +0700

    [WIP] Add normalization info

--- quantAwareTrain_v1.py
+++ quantAwareTrain_v1.py
@@ -299,14 +299,14 @@ def mainQuantAware(mnist=True):
             datasets.MNIST('../data', train=True, download=True,
                            transform=transforms.Compose([
                                transforms.ToTensor(),
-                               transforms.Normalize((0.1307,), (0.3081,))
+                               transforms.Normalize((0.1307,), (0.3081,))  # Normalize (mean, stdev)
                            ])),
             batch_size=batch_size, shuffle=True, **kwargs)
 
         test_loader = torch.utils.data.DataLoader(
             datasets.MNIST('../data', train=False, transform=transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
+                transforms.Normalize((0.1307,), (0.3081,))  # Normalize (mean, stdev)
             ])),
             batch_size=test_batch_size, shuffle=True, **kwargs)
     else:
